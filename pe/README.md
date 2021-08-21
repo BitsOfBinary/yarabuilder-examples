88cd88a247fcbb51378d0e0794129d1c08968b6a
BitsOfBinary <48534004+BitsOfBinary@users.noreply.github.com>
2021-08-21 15:14:33 +0100

    Added support for multiple PEs

--- pe/README.md
+++ pe/README.md
@@ -1,15 +1,15 @@
 # yarabuilder_pe.py
-Automatically parse metadata (import hash, rich header hash, PDB path) from a PE file, and create a dummy rule to be refined manually. The underlying class is also designed to let you create multiple rules at once if you have many PE files to signature.
+Automatically parse metadata (import hash, rich header hash, PDB path) from a PE file (or multiple files), and create a YARA rule of those values. The underlying class is also designed to let you create multiple rules at once if you have many PE files to signature.
 
 ## Usage
 ```
 usage: yarabuilder_pe.py [-h] --input INPUT [--rulename RULENAME]
 
-Generate YARA rules from metadata of a PE file
+Generate YARA rules from the metadata of PE files
 
 optional arguments:
   -h, --help           show this help message and exit
-  --input INPUT        PE file to generate rule for
+  --input INPUT        PE file(s) to generate rule for (accepts wildcards)
   --rulename RULENAME  (Optional) name for the generated YARA rule
 ```
 
--- pe/yarabuilder_pe.py
+++ pe/yarabuilder_pe.py
@@ -9,6 +9,8 @@ import argparse
 import hashlib
 import pefile
 import yarabuilder
+import glob
+import logging
 
 
 class YaraBuilderPE(yarabuilder.YaraBuilder):
@@ -19,37 +21,28 @@ class YaraBuilderPE(yarabuilder.YaraBuilder):
     def __init__(self):
         yarabuilder.YaraBuilder.__init__(self)
 
+        self.imphashes = []
+        self.rich_headers_hashes = []
+        self.pdbs = []
         self.rule_id = 1
-
-    def pe_to_yara(self, pe, rule_name=None):
-        """
-        Main method to call get features of the PE, and then create the YARA rules
-        """
-
-        if not rule_name:
-            rule_name = "generated_pe_rule_" + str(self.rule_id)
-            self.rule_id += 1
-
-        self.create_rule(rule_name)
-        self.add_import(rule_name, "pe")
-        self.add_import(rule_name, "hash")
-
-        condition = "uint16(0) == 0x5A4D and ("
-
-        # Import hash
-        imphash = pe.get_imphash()
-
-        condition += 'pe.imphash() == "{}"'.format(imphash)
-
-        # Rich header hash
+        
+    def get_pe_sha256_hash(self, pe):
+        return hashlib.sha256(pe.__data__).hexdigest()
+        
+    def parse_imphash(self, pe):
+        try:
+            imphash = pe.get_imphash()
+            self.imphashes.append(imphash)
+        except:
+            pass
+            
+    def parse_rich_header_hash(self, pe):
         if pe.RICH_HEADER:
             rich_header_hash = hashlib.md5(pe.RICH_HEADER.clear_data).hexdigest()
-
-            condition += ' or hash.md5(pe.rich_signature.clear_data) == "{}"'.format(
-                rich_header_hash
-            )
-
-        # PDB path
+            
+            self.rich_headers_hashes.append(rich_header_hash)
+            
+    def parse_pdb(self, pe):
         if hasattr(pe, "DIRECTORY_ENTRY_DEBUG"):
             if pe.DIRECTORY_ENTRY_DEBUG[0].entry:
                 if pe.DIRECTORY_ENTRY_DEBUG[0].entry.PdbFileName:
@@ -59,31 +52,95 @@ class YaraBuilderPE(yarabuilder.YaraBuilder):
                         pdb_path.replace(b"\x00", b"").decode().replace("\\", "\\\\")
                     )
 
-                    self.add_text_string(rule_name, pdb_path, name="pdb_path")
-
-                    condition += " or any of them"
-
+                    self.pdbs.append(pdb_path)
+        
+    def parse_pe(self, pe_filepath, rule_name):
+        try:
+            pe = pefile.PE(pe_filepath)
+        except pefile.PEFormatError:
+            logging.warning("{} is not a PE".format(pe_filepath))
+            return
+            
+        pe_hash = self.get_pe_sha256_hash(pe)
+        
+        self.add_meta(rule_name, "hash", pe_hash)
+           
+        self.parse_imphash(pe)
+        self.parse_rich_header_hash(pe)
+        self.parse_pdb(pe)
+        
+                    
+    def build_pe_condition(self, rule_name):
+        
+        condition = "uint16(0) == 0x5A4D and ("
+        
+        imphash_str = ""
+        if self.imphashes:
+            
+            for imphash in self.imphashes[:-1]:
+                imphash_str += 'pe.imphash() == "{}" or '.format(imphash)
+                
+            imphash_str += 'pe.imphash() == "{}"'.format(self.imphashes[-1])
+        
+        rich_header_hash_str = ""
+        if self.rich_headers_hashes:
+            
+            for rich_header_hash in self.rich_headers_hashes[:-1]:
+                rich_header_hash_str += 'hash.md5(pe.rich_signature.clear_data) == "{}" or '.format(rich_header_hash)
+                
+            rich_header_hash_str += 'hash.md5(pe.rich_signature.clear_data) == "{}"'.format(self.rich_headers_hashes[-1])
+        
+        pdb_str = ""
+        if self.pdbs:
+            pdb_str = "any of them"
+            pdb_id = 0
+            
+            for pdb in self.pdbs:
+                self.add_text_string(rule_name, pdb, name="pdb_path{}".format(pdb_id))
+                pdb_id += 1
+                
+        condition += ' or '.join(filter(None, [imphash_str, rich_header_hash_str, pdb_str]))
+        
         condition += ")"
 
         self.add_condition(rule_name, condition)
+            
+
+    def pe_to_yara(self, input_pes, rule_name=None):
+        """
+        Main method to call get features of the PE, and then create the YARA rules
+        """
+
+        if not rule_name:
+            rule_name = "generated_pe_rule_" + str(self.rule_id)
+            self.rule_id += 1
+            
+        self.create_rule(rule_name)
+        self.add_import(rule_name, "pe")
+        self.add_import(rule_name, "hash")
+
+        for pe_filepath in input_pes:
+            self.parse_pe(pe_filepath, rule_name)
+
+        self.build_pe_condition(rule_name)
 
 
 parser = argparse.ArgumentParser(
-    description=("Generate YARA rules from metadata of a PE file")
+    description=("Generate YARA rules from the metadata of PE files")
 )
 parser.add_argument(
-    "--input", type=str, help="PE file to generate rule for", required=True
+    "--input", type=str, help="PE file(s) to generate rule for (accepts wildcards)", required=True
 )
 parser.add_argument(
     "--rulename", type=str, help="(Optional) name for the generated YARA rule"
 )
 args = parser.parse_args()
 
-input_pe = pefile.PE(args.input)
+input_pes = glob.glob(args.input)
 
 pe_yarabuilder = YaraBuilderPE()
 
-pe_yarabuilder.pe_to_yara(input_pe, rule_name=args.rulename)
+pe_yarabuilder.pe_to_yara(input_pes, rule_name=args.rulename)
 
 rules = pe_yarabuilder.build_rules()
 
